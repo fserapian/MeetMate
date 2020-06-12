ef8ec339c190cd4365e0b7374520d3a757fc4a3d
Fadi Serapian <fadi.serapian@gmail.com>
2020-06-12 12:24:53 -0400

    set up routing for added components

--- MeetMate-SPA/src/app/app.module.ts
+++ MeetMate-SPA/src/app/app.module.ts
@@ -12,15 +12,21 @@ import { AuthService } from "./services/auth.service";
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, RegisterComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, RegisterComponent, MemberListComponent, ListsComponent, MessagesComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
